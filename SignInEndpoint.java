d3b974068885c5ab5c19c363b24456522f161b72
pedroigor <pigor.craveiro@gmail.com>
2012-09-13 14:25:35 -0300

    Initial code for the authentication using PicketBox CDI.

---
+++ SignInEndpoint.java
@@ -0,0 +1,103 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.aerogear.todo.server.security.authc;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.aerogear.todo.server.security.AuthenticationRequest;
+import org.aerogear.todo.server.security.AuthenticationResponse;
+import org.jboss.picketlink.cdi.Identity;
+import org.jboss.picketlink.cdi.credential.Credential;
+import org.jboss.picketlink.cdi.credential.LoginCredentials;
+import org.picketbox.cdi.PicketBoxUser;
+import org.picketbox.core.authentication.credential.UsernamePasswordCredential;
+
+/**
+ * <p>JAX-RS Endpoint to authenticate users.</p>
+ * 
+ * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
+ *
+ */
+@Stateless
+@Path("/signin")
+public class SignInEndpoint {
+
+    @Inject
+    private Identity identity;
+    
+    @Inject
+    private LoginCredentials credential;
+    
+    /**
+     * <p>Performs the authentication using the informations provided by the {@link AuthenticationRequest}</p>
+     * 
+     * @param authcRequest
+     * @return
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public AuthenticationResponse login(final AuthenticationRequest authcRequest) {
+        if (this.identity.isLoggedIn()) {
+            return createResponse(authcRequest);
+        }
+        
+        credential.setUserId(authcRequest.getUserId());
+        credential.setCredential(new Credential<UsernamePasswordCredential>() {
+
+            @Override
+            public UsernamePasswordCredential getValue() {
+                return new UsernamePasswordCredential(authcRequest.getUserId(), authcRequest.getPassword());
+            }
+        });
+        
+        this.identity.login();
+
+        return createResponse(authcRequest);
+    }
+
+    private AuthenticationResponse createResponse(AuthenticationRequest authcRequest) {
+        AuthenticationResponse response = new AuthenticationResponse();
+        
+        response.setUserId(authcRequest.getUserId());
+        
+        boolean isLoggedIn = this.identity.isLoggedIn();
+        
+        response.setLoggedIn(isLoggedIn);
+        
+        if (isLoggedIn) {
+            PicketBoxUser user = (PicketBoxUser) this.identity.getUser();
+            
+            response.setToken(user.getSubject().getSession().getId().getId().toString());
+        }
+        
+        return response;
+    }
+    
+}
