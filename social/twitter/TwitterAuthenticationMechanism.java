bc015a26c7ee6868f5db8860e5e725a425711511
Anil Saldhana <Anil.Saldhana@jboss.com>
2012-10-03 15:47:52 -0500

    minor changes and start of twitter login

---
+++ social/twitter/TwitterAuthenticationMechanism.java
@@ -0,0 +1,165 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.aerogear.todo.server.security.authc.social.twitter;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.jboss.picketlink.idm.IdentityManager;
+import org.jboss.picketlink.idm.model.Group;
+import org.jboss.picketlink.idm.model.Role;
+import org.jboss.picketlink.idm.model.User;
+import org.picketbox.core.Credential;
+import org.picketbox.core.authentication.AuthenticationInfo;
+import org.picketbox.core.authentication.AuthenticationManager;
+import org.picketbox.core.authentication.AuthenticationResult;
+import org.picketbox.core.authentication.impl.AbstractAuthenticationMechanism;
+import org.picketbox.core.exceptions.AuthenticationException;
+import org.picketlink.social.standalone.fb.FacebookPrincipal;
+import org.picketlink.social.standalone.fb.FacebookProcessor;
+
+/**
+ * An authentication mechanism for Twitter SignIn
+ * 
+ * @author Anil Saldhana
+ * @author Pedro Silva
+ */
+public class TwitterAuthenticationMechanism extends AbstractAuthenticationMechanism {
+
+    private static final String TWIT_AUTH_STATE_SESSION_ATTRIBUTE = "TWIT_AUTH_STATE_SESSION_ATTRIBUTE";
+    protected String returnURL;
+    protected String clientID;
+    protected String clientSecret;
+    protected String scope = "email";
+
+    protected FacebookProcessor processor;
+    
+    protected IdentityManager identityManager;
+
+    public TwitterAuthenticationMechanism() {
+        clientID = System.getProperty("FB_CLIENT_ID");
+        clientSecret = System.getProperty("FB_CLIENT_SECRET");
+        returnURL = System.getProperty("FB_RETURN_URL");
+    }
+    
+    private enum STATES {
+        AUTH, AUTHZ, FINISH
+    }; 
+
+    public IdentityManager getIdentityManager() {
+        return identityManager;
+    }
+
+    public void setIdentityManager(IdentityManager identityManager) {
+        this.identityManager = identityManager;
+    }
+
+    
+    @Override
+    public List<AuthenticationInfo> getAuthenticationInfo() {
+        ArrayList<AuthenticationInfo> info = new ArrayList<AuthenticationInfo>();
+
+        info.add(new AuthenticationInfo("oAuth Authentication", "Provides oAuth authentication.", TwitterCredential.class));
+
+        return info;
+    }
+
+    /* (non-Javadoc)
+     * @see org.picketbox.core.authentication.impl.AbstractAuthenticationMechanism#doAuthenticate(org.picketbox.core.authentication.AuthenticationManager, org.picketbox.core.Credential, org.picketbox.core.authentication.AuthenticationResult)
+     */
+    @Override
+    protected Principal doAuthenticate(AuthenticationManager authenticationManager, Credential credential,
+            AuthenticationResult result) throws AuthenticationException {
+        TwitterCredential oAuthCredential = (TwitterCredential) credential;
+        
+        HttpServletRequest request = oAuthCredential.getRequest();
+        HttpServletResponse response = oAuthCredential.getResponse();
+        HttpSession session = request.getSession();
+
+        Principal principal = null;
+        
+        if (isFirstInteraction(session)) {
+            try {
+                getFacebookProcessor().initialInteraction(request, response);
+            } catch (IOException e) {
+                throw new AuthenticationException("Error while initiating Facebook authentication interaction.", e);
+            }
+        } else if (isAuthenticationInteraction(session)) {
+            getFacebookProcessor().handleAuthStage(request, response);
+        } else if (isAuthorizationInteraction(session)) {
+            session.removeAttribute(TWIT_AUTH_STATE_SESSION_ATTRIBUTE);
+            principal = getFacebookProcessor().getPrincipal(request, response);
+            checkUserInStore((FacebookPrincipal) principal);
+        }
+        
+        return principal;
+    }
+
+    private boolean isAuthorizationInteraction(HttpSession session) {
+        return getCurrentAuthenticationState(session).equals(STATES.AUTHZ.name());
+    }
+
+    private boolean isAuthenticationInteraction(HttpSession session) {
+        return getCurrentAuthenticationState(session).equals(STATES.AUTH.name());
+    }
+
+    private boolean isFirstInteraction(HttpSession session) {
+        return getCurrentAuthenticationState(session) == null || getCurrentAuthenticationState(session).isEmpty();
+    }
+
+    private String getCurrentAuthenticationState(HttpSession session) {
+        return (String) session.getAttribute(TWIT_AUTH_STATE_SESSION_ATTRIBUTE);
+    }
+
+    @SuppressWarnings("unchecked")
+    private FacebookProcessor getFacebookProcessor() {
+        if (this.processor == null) {
+            this.processor = new FacebookProcessor(clientID, clientSecret, scope, returnURL, Collections.EMPTY_LIST);
+        }
+        return this.processor;
+    }
+    
+    private void checkUserInStore(FacebookPrincipal principal){
+        if(identityManager != null){
+            User storedUser = identityManager.getUser(principal.getName());
+            if(storedUser == null){
+                User newUser = identityManager.createUser(principal.getName());
+                newUser.setFirstName(principal.getFirstName());
+                newUser.setLastName(principal.getLastName());
+                newUser.setEmail(principal.getEmail());
+
+                Role guest = this.identityManager.createRole("guest");
+                Group guests = identityManager.createGroup("Guests");
+
+                identityManager.grantRole(guest, newUser, guests);
+            }
+        }
+    }
+}
---
+++ social/twitter/TwitterSignInEndpoint.java
@@ -0,0 +1,152 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.aerogear.todo.server.security.authc.social.twitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.aerogear.todo.server.security.authc.AuthenticationResponse;
+import org.jboss.picketlink.cdi.Identity;
+import org.jboss.picketlink.cdi.credential.Credential;
+import org.jboss.picketlink.cdi.credential.LoginCredentials;
+import org.jboss.picketlink.idm.IdentityManager;
+import org.jboss.picketlink.idm.model.User;
+import org.picketbox.cdi.PicketBoxCDISubject;
+import org.picketbox.cdi.PicketBoxUser;
+import org.picketbox.core.PicketBoxSubject;
+import org.picketlink.social.standalone.oauth.OpenIdPrincipal;
+
+/**
+ * Enables signin with facebook
+ * 
+ * @author anil saldhana
+ * @since Sep 19, 2012
+ */
+@Stateless
+@Path("/twitter")
+public class TwitterSignInEndpoint {
+
+    @Inject
+    private Identity identity;
+
+    @Inject
+    private LoginCredentials credential;
+
+    @Inject
+    private IdentityManager identityManager;
+
+    @GET
+    public String login(@Context final HttpServletRequest request, @Context final HttpServletResponse response) throws IOException {
+        if (this.identity.isLoggedIn()) {
+            return null;
+        }
+        
+        this.credential.setCredential(new Credential<TwitterCredential>() {
+
+            @Override
+            public TwitterCredential getValue() {
+                return new TwitterCredential(request, response);
+            }
+        });
+        
+        this.identity.login();
+        
+        if (this.identity.isLoggedIn()) {
+            provisionNewUser();
+            return "<script>window.opener.sendMainPage();</script>";
+        }
+        
+        return null;
+    }
+
+    /**
+     * <p>Provision the authenticated user if he is not stored yes.</p>
+     * 
+     * TODO: user provisioning feature should be provided by PicketBox ? 
+     */
+    private void provisionNewUser() {
+        OpenIdPrincipal openIDPrincipal = getAuthenticatedPrincipal();
+        
+        //Check if the user exists in DB
+        User storedUser = identityManager.getUser(openIDPrincipal.getName());
+        if(storedUser == null){
+            storedUser = identityManager.createUser(openIDPrincipal.getFullName());
+
+            storedUser = identityManager.createUser(openIDPrincipal.getFullName());
+            storedUser.setFirstName(openIDPrincipal.getFirstName());
+            storedUser.setLastName(openIDPrincipal.getLastName());
+            storedUser.setEmail(openIDPrincipal.getEmail()); 
+        }
+        ArrayList<String> roles = new ArrayList<String>();
+        
+        /*Role guest = this.identityManager.createRole("guest");
+        Group guests = identityManager.createGroup("Guests");
+
+        identityManager.grantRole(guest, storedUser, guests);*/
+        // necessary because we need to show the user info at the main page. Otherwise the informations will be show only after the second login.
+        PicketBoxUser user = (PicketBoxUser) identity.getUser();
+        PicketBoxCDISubject subject = user.getSubject();
+        
+        subject.setIdmUser(storedUser);
+        
+        subject.setRoleNames(roles);
+    }
+
+    private OpenIdPrincipal getAuthenticatedPrincipal() {
+        PicketBoxUser user = (PicketBoxUser) identity.getUser();
+        PicketBoxSubject subject = user.getSubject();
+        
+        return (OpenIdPrincipal) subject.getUser();
+    }
+
+    private AuthenticationResponse createSuccessfulAuthResponse() {
+        AuthenticationResponse response = new AuthenticationResponse();
+        
+        response.setLoggedIn(this.identity.isLoggedIn());
+        
+        return response;
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public AuthenticationResponse getStatus(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException{
+        if(identity.isLoggedIn()){
+            User user = identity.getUser();
+            AuthenticationResponse authResponse = createSuccessfulAuthResponse();
+            authResponse.setLoggedIn(true);
+            authResponse.setToken(user.getId());
+            return authResponse;
+        }
+        return null;
+    }
+}
